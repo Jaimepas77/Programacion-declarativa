bfc8ed52748a3bec6d738ac6a4810b0e82346def
Jaime <79403778+Jaimepas77@users.noreply.github.com>
2021-11-23 20:50:09 +0100

    lab5 terminado en casa
    lab5 con el ejercicio 2 mejorado y el 3 completado

--- lab5.hs
+++ lab5.hs
@@ -20,24 +20,33 @@ data Matriz = M[[Float]]
 leerM::IO Matriz    --Se ve forzadpo a leer nºs de línea en línea
 leerM = do  putStr("Introduzca el nº de filas que tendrá su matriz: ")
             x <- getInt
-            putStr("Introduzca el nº de columnas que tendrá su matriz: ")
-            y <- getInt
-            putStrLn("Introduzca la matriz poniendo un nº por línea")
-            leerFilas x y []
+            --putStr("Introduzca el nº de columnas que tendrá su matriz: ")
+            --y <- getInt
+            putStrLn("Introduzca la matriz poniendo una fila por línea")
+            leerFilas x []
 
 getFloat::IO Float
 getFloat = do   line <- getLine
                 return (read line::Float)
 
-leerFilas::Int -> Int -> [[Float]] -> IO Matriz
-leerFilas x y m = do    f <- leerLinea y []
-                        if x == 1 then return $M (m++[f])
-                            else leerFilas (x-1) y (m++[f])
+leerFilas::Int -> [[Float]] -> IO Matriz
+leerFilas x m = do  f <- leerLinea []
+                    if x == 1 then return $M (m++[f])
+                        else leerFilas (x-1) (m++[f])
 
-leerLinea::Int -> [Float] -> IO [Float]
+leerLinea::[Float] -> IO [Float]
+leerLinea ys = do   xs <- getLine
+                    return $strToFloat $words xs
+
+strToFloat::[String] -> [Float]
+strToFloat [] = []
+strToFloat (x:xs) = (read x::Float):(strToFloat xs)
+
+{---Antiguo
 leerLinea y ys = do e <- getFloat     
                     if y == 1 then return (ys ++ [e])
                     else leerLinea (y-1) (ys++[e])
+-}
 
 --b
 dibujaMatriz:: Matriz -> IO()
@@ -58,4 +67,23 @@ actuar = do m <- leerM
 
 --3
 formatea::String -> String -> Int -> IO ()
-formatea fileIn fileOut n = do print("ERROR 404: SOLUTION NOT FOUND")--Pendiente de implementar
+formatea fileIn fileOut n = do  text <- readFile fileIn
+                                --print("ERROR 404: SOLUTION NOT FOUND")--Pendiente de implementar
+                                writeFile fileOut $justifica (lines text) n
+
+justifica::[String] -> Int -> String
+justifica [] _ = ""
+justifica (l:ls) n = if huecos >= 1 && caracteres < n then
+                            (concatenar (words l) ((n-caracteres) `div` huecos)) ++ "\n" ++ justifica ls n
+                        else "\n" ++ justifica ls n
+                    where   huecos = (length(words l)-1)
+                            caracteres = length (concat(words l))
+
+concatenar::[String] -> Int -> String
+concatenar [word] _ = word
+concatenar (word:ws) s = word++(espacios s)++(concatenar ws s)
+concatenar [] _ = ""
+
+espacios::Int -> String
+espacios n = if n > 0 then ' ':(espacios (n-1))
+                else " "
